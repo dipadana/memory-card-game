8d1076bbd9c350da6765ebb2dd00ae0748e754b3
dipadana <dipadanadev@gmail.com>
2021-10-24 11:06:42 +0800

    Basic logic done

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-let card1 = 0;
-let card2 = 0;
+let card1 = "";
+let card2 = "";
 
-function cardClicked(value) {
-  if (card1 === 0) {
-    card1 = value;
+async function cardClicked(element) {
+  if (element.target.getAttribute("clicked") === "true") return;
+
+  if (card1 === "") {
+    card1 = element.target;
+    card1.setAttribute("class", "frontClicked");
+    card1.setAttribute("clicked", "true");
   } else {
-    card2 = value;
+    card2 = element.target;
+    card2.setAttribute("class", "frontClicked");
+    card2.setAttribute("clicked", "true");
   }
 
   if (card1 && card2) {
@@ -14,12 +20,21 @@ function cardClicked(value) {
 }
 
 function cardChecker() {
-  if (card1 === card2) {
+  if (card1.getAttribute("name") === card2.getAttribute("name")) {
+    card1.setAttribute("class", "frontTrue");
+    card2.setAttribute("class", "frontTrue");
+    card1 = "";
+    card2 = "";
     console.log("card match");
-    let card = document.querySelector(`.front[${card1}]`);
   } else {
     console.log("card not match");
+    setTimeout(() => {
+      card1.setAttribute("class", "front");
+      card2.setAttribute("class", "front");
+      card1.setAttribute("clicked", "false");
+      card2.setAttribute("clicked", "false");
+      card1 = "";
+      card2 = "";
+    }, 200);
   }
-  card1 = 0;
-  card2 = 0;
 }
