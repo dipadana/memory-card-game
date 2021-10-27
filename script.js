44a529d491a059c07b5a963a4cb307125c07aa60
dipadana <dipadanadev@gmail.com>
2021-10-27 20:32:18 +0800

    Add logic flip card

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const arrayData = [1, 2, 3];
+const arrayData = [1, 2, 3, 4, 6, 7, 8, 9];
 
 let card1 = "";
 let card2 = "";
@@ -6,13 +6,16 @@ let card2 = "";
 async function cardClicked(element) {
   if (element.target.getAttribute("clicked") === "true") return;
 
+  //developer.mozilla.org/en-US/docs/Web/API/Event/composedPath
+  //developer.mozilla.org/en-US/docs/Web/API/Element/classList
+
   if (card1 === "") {
-    card1 = element.target;
-    card1.setAttribute("class", "frontClicked");
+    card1 = element.composedPath()[1];
+    card1.classList.add("flipped-card");
     card1.setAttribute("clicked", "true");
   } else {
-    card2 = element.target;
-    card2.setAttribute("class", "frontClicked");
+    card2 = element.composedPath()[1];
+    card2.classList.add("flipped-card");
     card2.setAttribute("clicked", "true");
   }
 
@@ -23,31 +26,53 @@ async function cardClicked(element) {
 
 function cardChecker() {
   if (card1.getAttribute("name") === card2.getAttribute("name")) {
-    card1.setAttribute("class", "frontTrue");
-    card2.setAttribute("class", "frontTrue");
     card1 = "";
     card2 = "";
     console.log("card match");
   } else {
     console.log("card not match");
     setTimeout(() => {
-      card1.setAttribute("class", "front");
-      card2.setAttribute("class", "front");
+      card1.classList.remove("flipped-card");
+      card2.classList.remove("flipped-card");
       card1.setAttribute("clicked", "false");
       card2.setAttribute("clicked", "false");
       card1 = "";
       card2 = "";
-    }, 200);
+    }, 300);
   }
+}
+
+function mixArray(a, b, c, d) {
+  //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  //Fisher-Yates shuffle algorithm
+  //array,placeholder,placeholder,placeholder
+
+  c = a.length;
+  while (c)
+    (b = (Math.random() * (--c + 1)) | 0),
+      (d = a[c]),
+      (a[c] = a[b]),
+      (a[b] = d);
 }
 
 function displayCard() {
   const doubledArrayData = [...arrayData, ...arrayData];
-  const container = document.querySelector(".container");
+  mixArray(doubledArrayData);
+  const container = document.querySelector(".card-container");
   doubledArrayData.forEach((value) => {
-    container.innerHTML += `<div class="front" name="${value}" clicked="false" onclick="cardClicked(event)">
-          ${value}
-        </div>`;
+    container.innerHTML += `
+      <div class="card">
+        <div
+          class="card-inner"
+          name="${value}"
+          clicked="false"
+          onclick="cardClicked(event)"
+        >
+          <div class="card-front">${value}</div>
+          <div class="card-back"></div>
+        </div>
+      </div>
+    `;
   });
 }
 displayCard();
